2584b01a50c57a5aceb15ec440ad8d96cd346c0b
Clément Vacandard <clementvacandard@MacBook-Pro-de-Clement-2.local>
2017-06-24 20:03:14 +0200

    Reucperation image serveur

--- app/src/main/java/com/example/clementvacandard/banqueimages/MainActivity.java
+++ app/src/main/java/com/example/clementvacandard/banqueimages/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.clementvacandard.banqueimages;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +24,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
        rechercher_btn = (Button) findViewById(R.id.rechercher_btn);
         log_tv = (TextView) findViewById(R.id.log_tv);
@@ -32,21 +44,29 @@ public class MainActivity extends Activity {
     }
 
     public void rechercherImage(View v) {
-        String numero = numeroImage_tf.getText();
+        String numero = numeroImage_tf.getText().toString();
         log_tv.setText("Recherche de l'image "+numero);
 
         String src = "http://192.168.1.15:8888/image.php?face="+numero;
+        System.out.println(src);
+        //String src = "http://www.androidbegin.com/wp-content/uploads/2013/07/HD-Logo.gif";
+        Bitmap bitmap = null;
 
+        try{
+            java.net.URL url = new java.net.URL(src);
+            InputStream input = (InputStream) url.getContent();
+            bitmap = BitmapFactory.decodeStream(input);
+        }catch (IOException e) {
+            log_tv.setText(e.getMessage());
+            System.out.println(e.getMessage());
+        }
 
-
-        afficherImage();
+        afficherImage(bitmap);
     }
 
-    public void afficherImage(){
-
+    public void afficherImage(Bitmap bitmap){
         ImageView view = (ImageView) findViewById(R.id.imageView);
-        view.setImageResource(R.drawable.grenouille);
-
+        view.setImageBitmap(bitmap);
     }
 
 
